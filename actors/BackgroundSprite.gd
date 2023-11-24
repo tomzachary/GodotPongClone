80d69ed7c326ce0100982fc97616bdbbe47db540
thms1489 <tomzachary@live.com>
2023-11-24 13:50:12 -0600

    figured out player position setup on main game

---
+++ actors/BackgroundSprite.gd
@@ -0,0 +1,12 @@
+extends Sprite2D
+var screen_size
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	scale = screen_size * 2
+	pass
---
+++ actors/main_game.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+var screen_size
+var player
+var pong
+var enemy
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+	player = $Player
+	player.position = Vector2(screen_size.x - 70, 0)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
